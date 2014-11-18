bbd61d28450c91bd1e8fe5e0c509063b96c3fe87
Kcser <kcsers@gmail.com>
2014-11-18 15:37:32 +0800

    get_in

---
+++ test/get_in_terrain.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<boost/algorithm/string.hpp>
+#include<boost/lexical_cast.hpp>
+#include<fstream>
+#include "../inc/head.h"
+
+typedef std::string string;
+
+
+int split(const string line, std::vector<POINT_LATLON_Z> *points)
+{
+	std::vector<string> str;
+	POINT_LATLON_Z point;
+	boost::split(str, line, boost::is_any_of(" "));
+	point.coord.lon_deg = boost::lexical_cast<double>(*str.begin());
+	point.coord.lat_deg = boost::lexical_cast<double>(*(str.begin()+1));
+	point.z = boost::lexical_cast<double>(*(str.begin()+2));
+	points->push_back(point);
+	return 0;
+}
+
+
+
+
+
+
+
+int get_in_terrain(const char *in_file, std::vector<POINT_LATLON_Z> *points)
+{
+	std::ifstream in(in_file);
+	string input;
+	while(getline(in, input)){
+		split(input, points);
+	}
+	return 0;
+}
+
+int test_output(std::vector<POINT_LATLON_Z> *points)
+{
+	for(std::vector<POINT_LATLON_Z>::iterator it = points->begin();
+			it != points->end();
+			++it)
+	{
+		std::cout<<it->coord.lon_deg<<std::endl;
+		std::cout<<it->z<<std::endl;
+
+	}
+	return 0;
+}
+
+
+int main()
+{
+	std::vector<POINT_LATLON_Z> points;
+	get_in_terrain("../data/terrain/31.8_117.2", &points);
+	test_output(&points);
+	return 0;
+}
+
