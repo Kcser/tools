7a955b038e904e7e9fc1df94e7442eba575eb07d
Kcser <kcsers@gmail.com>
2014-11-18 10:57:15 +0800

    nest_index

---
+++ src/nest_index.cpp
@@ -0,0 +1,30 @@
+#include "../inc/head.h"
+
+
+int *nest_index(int length, int height)
+{
+	int *index;
+	int index_len = 8*(length - 1)*(height - 1);
+	index = new int[index_len];
+	int p_index = 0;
+	for(int i = 0;i < height - 1; i ++)
+	{
+		for(int j = 0; j < length - 1; j++)
+		{
+			index[p_index] = i * length + j;
+			index[p_index+1] = (i + 1)*length + j;
+			index[p_index+2] = (i + 1)*length + j + 1;
+			index[p_index+3] = -1;
+
+			index[p_index+4] = i * length + j;
+			index[p_index+5] = (i + 1)*length + j + 1;
+			index[p_index+6] = i * length + j + 1;
+			index[p_index+7] = -1;
+
+			p_index += 8;
+		}
+	}
+	return index;
+}
+
+
